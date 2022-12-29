371be4feba472807a8267cfcc168b34d42fa5cc5
Skhull.Coding <skhull.coding@gmail.com>
2022-12-29 20:13:27 +0530

    Make Rough Design (without responsive)

--- script/script-base.js
+++ script/script-base.js
@@ -1,6 +1,11 @@
 let prmNavBar = document.getElementById("mainNavBar");
 let secNavBar = document.getElementById("secondaryNavBar");
 let secNavButton = document.getElementById("secondaryNavBarLines");
+let cButton = document.getElementById("contact-submit");
+// let mC = document.getElementById("portfolioSections");
+// let t;
+
+cButton.addEventListener('click', (e)=>{e.preventDefault();})
 
 secNavButton.addEventListener('click', (e)=>{
     // console.log(secNavBar.lastElementChild.dataset.work);
@@ -19,6 +24,8 @@ secNavButton.addEventListener('click', (e)=>{
         prmNavBar.style.display = "none";
         secNavBar.lastElementChild.style.left = 0;
         secNavBar.lastElementChild.dataset.work = "true";
+        // clearTimeout(t);
+        // mC.style.zIndex = -1;
     }
     else{
         
@@ -31,6 +38,63 @@ secNavButton.addEventListener('click', (e)=>{
         prmNavBar.style.display = "initial";
         secNavBar.lastElementChild.style.left = "-100%";
         secNavBar.lastElementChild.dataset.work = "false";
+        // t = setTimeout(() => {
+        //     mC.style.zIndex = "initial";
+        // }, 450);
     }
     // console.log
+})
+
+let a = fetch("https://api.github.com/users/skhull-coding/repos");
+
+a.then((v)=>{
+    // console.log(v.json());
+    return v.json();
+}).then((v)=>{
+    let toADD = `<div class="projectBox">
+    <h3 class="projectTitle">ptitle</h3>
+    <p class="projectDesc">pdesc</p>
+    <div class="languages"><h4>languages :</h4>
+        
+        </div>
+    <a href="prepo" class="mt"><button class="repolink">Github Repo</button></a>
+    <a href="pdeploy"><button class="deployButton">View deployment</button></a>
+
+</div>`;
+    // console.log(v);
+    
+    let toClasses = document.getElementsByClassName("languages");
+    let n = 0;
+    v.forEach(element => {
+        // console.log(element.html_url);
+        // console.log(element.name)
+        // if (element.description){
+        // console.log(element.description);}
+        let l = fetch(element.languages_url);
+        l.then(v=>v.json()).then(v=>{
+            console.log(v);
+            let sum = 0;
+            for (item in v){
+                sum += v[item];
+            }
+            
+            if (sum ==0){
+                toClasses[n].innerHTML += "<span>No Languages used</span>";
+                n++;
+                return;
+            }
+            console.log(sum);
+            for (item in v){
+                
+            toClasses[n].innerHTML += `
+            <span>${item} -  ${Math.floor(v[item]/sum * 100)}%</span>
+            <div class="languageUsed"><div class="from" style="width: ${Math.ceil(v[item]/sum * 100)}%;"></div></div>`;
+            console.log(v[item]/sum);
+            }
+            n++;
+        })
+        // console.log("https://" + element.owner.login + ".github.io/"  + element.name)
+
+        document.querySelector("#projects .sectionContent").innerHTML += toADD.replace("ptitle", element.name).replace("pdesc", element.description?(element.description) : "No Description").replace("prepo", element.html_url).replace("pdeploy", "https://" + element.owner.login + ".github.io/"  + element.name);
+    });
 })
